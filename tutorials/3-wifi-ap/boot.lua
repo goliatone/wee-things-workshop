3eea1de7862869f8e91736d627fb2f23172d5d0c
goliatone <burgosemi@gmail.com>
2015-11-17 23:15:27 -0500

    moving things around

---
+++ tutorials/3-wifi-ap/boot.lua
@@ -0,0 +1,28 @@
+-- Initialize WiFi access point
+wifi.setmode(wifi.SOFTAP)
+
+-- Configure the access point
+cfg = {
+    ssid = "WEE_THINGS_"..node.chipid(),
+    pwd = "weething"
+}
+wifi.ap.config(cfg)
+
+print('Wifi mode: ', wifi.getmode())
+print('MAC: ', wifi.sta.getmac())
+print('')
+print('Created WiFi network:')
+print('Network Name')
+print('- SSID: ', cfg.ssid)
+print('Security: WPA/WPA 2 Personal')
+print('- PWD: ', cfg.pwd)
+print('')
+
+address = {
+  ip = '192.168.4.1',
+  netmask = '255.255.255.0',
+  gateway = '192.168.4.1'
+}
+wifi.ap.setip(address)
+
+print('IP: ', wifi.ap.getip())

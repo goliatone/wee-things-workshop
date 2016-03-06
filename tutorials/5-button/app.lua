11a39ac9447b136a6671a0ab513b32280e2c291e
goliatone <burgosemi@gmail.com>
2016-03-06 09:33:47 -0500

    5-button: update lua code

--- tutorials/5-button/app.lua
+++ tutorials/5-button/app.lua
@@ -1,8 +1,16 @@
 local App = {}
 
-function App.start()
+App.count = 0
 
+function onpindown(level)
+    App.count = App.count + 1
+    print("Pin down", level, App.count % 2)
+    gpio.write(0, App.count % 2)
 end
 
+function App.start()
+    gpio.mode(2, gpio.INPUT, gpio.PULLUP)
+    gpio.trig(2, 'both', onpindown)
+end
 
 App.start();
